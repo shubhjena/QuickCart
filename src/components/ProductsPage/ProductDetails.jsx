f02aa6029b3ccd92f81d7d757259baef150d7b85
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-19 04:43:37 +0530

    Create ProductDetails.jsx

---
+++ src/components/ProductsPage/ProductDetails.jsx
@@ -0,0 +1,88 @@
+const Productdetail =
+[
+    {
+        id:1,
+        title:"Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.",
+        category: 'Tablet',
+        price: '723',
+        image: 'https://source.unsplash.com/256x256/?Tablet'
+    },
+    {
+        id:2,
+        title:"Tracker with IP67 Waterproof Pedometer Smart watch.",
+        category: 'Smart Watch',
+        price: '168',
+        image: 'https://source.unsplash.com/256x256/?Smartwatch'
+    },
+    {
+        id:3,
+        title:"Noise Cancelling Headphones Wireless.",
+        category: 'Headphone',
+        price: '49',
+        image: 'https://source.unsplash.com/256x256/?Headphone'
+    },
+    {
+        id:4,
+        title:"Professional Camera 4K Digital Video Camera.",
+        category: 'Camera',
+        price: '1049',
+        image: 'https://source.unsplash.com/256x256/?Camera'
+    },
+    {
+        id:5,
+        title:"Mini Portable PD 22.5W Fast Charging Powerbank.",
+        category: 'Powerbank',
+        price: '49',
+        image: 'https://source.unsplash.com/256x256/?Powerbank'
+    },
+    {
+        id:6,
+        title:"CPU Cooler 2 Heat Pipes 12mm 4 Pin PWM RGB for Intel.",
+        category: 'Electronics',
+        price: '156',
+        image: 'https://source.unsplash.com/256x256/?cpu'
+    },
+    {
+        id:7,
+        title:"Playstation 4 2TB Slim Gaming Console.",
+        category: 'Gaming',
+        price: '2098',
+        image: 'https://source.unsplash.com/256x256/?playstation'
+    },
+    {
+        id:8,
+        title:"Mini Portable Mobile Phone Powerbank for iphone.",
+        category: 'Electronics',
+        price: '386',
+        image: 'https://source.unsplash.com/256x256/?powerbank'
+    },
+    {
+        id:9,
+        title:"Microsoft Surface Pro 8-13' Touchscreen.",
+        category: 'Tablet',
+        price: '693',
+        image: 'https://source.unsplash.com/256x256/?Tablet'
+    },
+    {
+        id:10,
+        title:"Playstation 4 2TB Slim Gaming Console.",
+        category: 'Gaming',
+        price: '5036',
+        image: 'https://source.unsplash.com/256x256/?playstation'
+    },
+    {
+        id:11,
+        title:"Echo Show 5 (2nd Gen) Adjustable Stand | Charcoal",
+        category: 'Electronics',
+        price: '198',
+        image: 'https://source.unsplash.com/256x256/?alexa'
+    },
+    {
+        id:12,
+        title:"Echo Dot smart speaker",
+        category: 'Electronics',
+        price: '793',
+        image: 'https://source.unsplash.com/256x256/?smartspeaker'
+    },
+]
+export default Productdetail;

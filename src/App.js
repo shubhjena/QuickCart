23c368c2fd6ef20906b82092fb9861cef3013cfb
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-19 04:44:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from './components/Nav.jsx'
-import Home from './components/HomePage/Home.jsx'
+import RouteComponent from './components/RouteComponent.jsx';
 import Footer from "./components/Footer.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Products from "./components/ProductsPage/ProductDetails"
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Productdetail from "./components/ProductsPage/ProductDetails";
 
 const App = () => {
+  //add to cart
+  const [cart,setCart] = useState([]);
+  const addToCart = (product) => 
+  { const exist = cart.find((item)=>{
+    return item.id===product.id
+  })
+    if(exist){
+      alert('Item already added to cart!')
+    }
+    else{
+      setCart([...cart,{...product,qty:1}])
+      alert('Success! Product added to cart.')
+    }
+  }
+
+  //filter products on product page
+  const[product,setProduct] = useState(Products);
+  const searchbtn = (searchCategory) =>
+  {
+    const update = Products.filter((currProduct) => 
+    {
+      return currProduct.category === searchCategory;
+    })
+    setProduct(update);
+  };
+
+  //display product using view product component
+  const [show, setShow] = useState([]);
+  const view = (product) => setShow([product]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Nav searchbtn={searchbtn} />
+        <RouteComponent product = {product} setProduct = {setProduct} show={show} setShow={setShow} view={view} cart={cart} setCart={setCart} addToCart={addToCart}/>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 };

0e2b90b26a1742edc32d6b994de406174da675f7
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-18 23:09:50 +0530

    Create ProductCarousel.jsx

---
+++ src/components/HomePage/ProductCarousel.jsx
@@ -0,0 +1,45 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import { ArrowRight,HeartFill } from 'react-bootstrap-icons';
+
+
+function ProductCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item className='bg-body-secondary'>
+      <img src="https://source.unsplash.com/1900x600/?Nature,Lightning"/>
+        <Carousel.Caption>
+          <h3>The Grandeur of Nature</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Button variant="primary" size="lg" className='mt-4 mb-5 px-5'>
+            Explore <ArrowRight/>
+          </Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="https://source.unsplash.com/1900x600/?flower,dark"/>
+        <Carousel.Caption>
+          <h3>Flower Fiesta</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Button variant="primary" size="lg" className='mt-4 mb-5 px-5'>
+            Explore <ArrowRight/>
+          </Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://source.unsplash.com/1900x600/?dark"/>   
+        <Carousel.Caption>
+          <h3>Dark is Nigh</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+          <Button variant="primary" size="lg" className='mt-4 mb-5 px-5'>
+            Explore <ArrowRight/>
+          </Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ProductCarousel;

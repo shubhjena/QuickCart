8ecc9c668311502afb1497b560ddc319159b3b7a
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-18 23:09:36 +0530

    Create CategoryCards.jsx

---
+++ src/components/HomePage/CategoryCards.jsx
@@ -0,0 +1,50 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
+function CategoryCards() {
+
+    const cards = [
+        {
+          title: 'Headphones',
+          text: 'This is the first object.',
+          image: 'https://source.unsplash.com/286x286/?headphone'
+        },
+        {
+          title: 'Smartphones',
+          text: 'This is the second object.',
+          image: 'https://source.unsplash.com/286x286/?smartphones'
+        },
+        {
+          title: 'Watches',
+          text: 'This is the third object.',
+          image: 'https://source.unsplash.com/286x286/?wristwatch'
+        },
+        {
+          title: 'Keyboards',
+          text: 'This is the fourth object.',
+          image: 'https://source.unsplash.com/286x286/?Keyboard-dark'
+        },
+        {
+          title: "Smart Watches",
+          text: 'This is the fifth object.',
+          image: 'https://source.unsplash.com/286x286/?smartwatch'
+        }
+      ];
+      
+  return (
+    <div className='d-flex'>
+        {cards.map((card,index)=>(
+            <Card key={index} style={{ width: '15rem' }} className="m-auto my-5 border-0" >
+            <Image src={card.image} roundedCircle />
+            <Card.Body className='text-center'>
+                <Card.Title className='mb-4'>{card.title}</Card.Title>
+                <Button variant="primary">Shop now!</Button>
+            </Card.Body>
+        </Card>
+        ))}
+    </div>
+  );
+}
+
+export default CategoryCards;

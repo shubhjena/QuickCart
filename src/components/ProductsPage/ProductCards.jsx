9058c28b66c90e49b527d4106857e9887cbc1aec
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-31 02:43:04 +0530

    fix: no action product card click
    add link to the product cards to open detailed product view on click.

--- src/components/ProductsPage/ProductCards.jsx
+++ src/components/ProductsPage/ProductCards.jsx
@@ -7,24 +7,26 @@ function ProductCards({products,view,addToCart,addToWishlist}) {
   return (
     <>
        {products.map((product,index)=>(
-        <div key={index} className='bg-alpha rounded-lg m-auto my-3 w-72 shadow ' onClick={()=>{view(product)}}>
-                <div className='m-auto flex flex-col p-3'>
+          <div key={index} className='bg-alpha rounded-lg m-auto my-3 w-72 shadow ' onClick={()=>{view(product)}}>
+            <div className='m-auto flex flex-col p-3'>
+              <Link to="/dp" className='m-auto flex flex-col text-black no-underline'>
                 <img src={product.image} className='rounded' />
-                    <span className=' text-lg font-semibold'>{product.title}</span>
-                    <span className='text-muted'>{product.category}</span>
-                    <div className='flex gap-3 text-gamma text-lg font-semibold cursor-pointer'>
-                      <span className=' text-accent font-bold mr-auto'>₹ {product.price}.00</span>
-                      {/*on click call addToCart function in App.js which appends product to cart state*/}
-                      <span onClick={()=>{addToCart(product)}} className='my-auto' title="Add to cart"><Cart4/></span>
+                <span className=' text-lg font-semibold'>{product.title}</span>
+                <span className='text-muted'>{product.category}</span>
+              </Link>
+              <div className='flex gap-3 text-gamma text-lg font-semibold cursor-pointer'>
+                <span className=' text-accent font-bold mr-auto'>₹ {product.price}.00</span>
+                {/*on click call addToCart function in App.js which appends product to cart state*/}
+                <span onClick={()=>{addToCart(product)}} className='my-auto' title="Add to cart"><Cart4/></span>
 
-                      {/*on click call addToWishlist function in App.js which appends product to wishlist state*/}
-                      <span onClick={()=>{addToWishlist(product)}} className='my-auto' title="Wish list"><SuitHeartFill/></span>
+                {/*on click call addToWishlist function in App.js which appends product to wishlist state*/}
+                <span onClick={()=>{addToWishlist(product)}} className='my-auto' title="Wish list"><SuitHeartFill/></span>
 
-                      {/*Link loads ViewProduct component*/}
-                      <Link to="/QuickCart/dp" className="text-gamma no-underline my-auto" title="View"><EyeFill/></Link>
-                    </div>
-                </div>
+                {/*Link loads ViewProduct component*/}
+                <Link to="/dp" className="text-gamma no-underline my-auto" title="View"><EyeFill/></Link>
+              </div>
             </div>
+          </div>
 
             //  Placeholder 
             // <Card key={index} style={{ width: '18rem' }} className='m-auto my-3'>
